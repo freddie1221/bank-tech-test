29366598e66f5c7f27a547a0a556b923a778bc6b
Freddie <fredjlawson@gmail.com>
2018-10-08 17:17:25 +0100

    feat: add account depositing & withdrawal

--- bank.js
+++ bank.js
@@ -3,4 +3,12 @@ function Account(){
   this.balance = 0
 }
 
+Account.prototype.deposit = function(amount){
+  this.balance += amount
+}
+
+Account.prototype.withdraw = function(amount){
+  this.balance -= amount
+}
+
 module.exports = Account
--- bank.test.js
+++ bank.test.js
@@ -1,11 +1,22 @@
-
 const Account = require("./bank")
+var account = new Account
 
-test('returns a balance', function(){
-  var account = new Account
-  expect(account.balance).toBe(0)
+
+describe('first tests', function(){
+  
+  test('returns a balance', function(){
+    expect(account.balance).toBe(0)
+  });
+
+  test('can make a deposit',() => {
+    account.deposit(10)
+    expect(account.balance).toBe(10)
+  });
+
+  test('can make a withdrawal',() => {
+    account.withdraw(20)
+    expect(account.balance).toBe(-10)
+  });
 });
 
-xtest('',() => {
 
-});
